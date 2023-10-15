4c88cac1a7136ecc0f1105be8d4fd3554d6f44d9
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-10-16 03:20:39 +0600

    added welcome section and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,13 @@
 import Banner from "@/components/home/Banner/Banner";
 import Features from "@/components/home/Features/Features";
 import Gallery from "@/components/home/Gallery/Gallery";
+import NavBar from "@/components/shared/NavBar";
 import AboutUs from "@/components/ui/AboutUs/AboutUs";
+import Footer from "@/components/ui/Footer/Footer"; 
+import Navbar from "@/components/ui/Navbar/Navbar";
+import Welcome from "@/components/ui/Welcome/Welcome";
 import PublicHeader from "@/components/view/Header/PublicHeader";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,9 +17,11 @@ export default function Home() {
       <PublicHeader />
       {/* <h1 className="text-l">This web application eLearning Platform</h1> */}
       <Banner />
-      <AboutUs />
+      <Welcome />
       <Features />
+      <AboutUs />
       <Gallery />
+      <Footer />
     </div>
   );
 }
