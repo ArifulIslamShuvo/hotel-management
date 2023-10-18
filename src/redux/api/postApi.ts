856cf0cb055170ed2dbf5b8b4fb75a2811c584e8
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-10-18 06:40:29 +0600

    update teatures section

--- src/redux/api/postApi.ts
+++ src/redux/api/postApi.ts
@@ -3,30 +3,6 @@
 
 import { baseApi } from "./baseApi";
 
-// // Define a service using a base URL and expected endpoints
-// export const postApi = createApi({
-//   reducerPath: "postApi",
-//   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
-//   endpoints: (builder) => ({
-
-//     getPostData: builder.query({
-//       query: () => "hotels",
-//     }),
-
-//     singlePostData: builder.query({
-//       query: (id) => ({
-//         url: `hotel/${id}`,
-//         method: "GET",
-//       }),
-//     }),
-
-//   }),
-// });
-
-// // Export hooks for usage in functional components, which are
-// // auto-generated based on the defined endpoints
-// export const { useGetPostDataQuery, useSinglePostDataQuery } = postApi;
-
 export const productApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getPostData: builder.query({
